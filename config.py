40da2f537c46c6ec017719a187e5cd9bc75124d0
Mariusz <mariusz.tober@gmail.com>
2024-03-10 22:05:41 +0100

    empty config file

---
+++ config.py
@@ -0,0 +1,8 @@
+username = ""
+password = ""
+# profile to scrape
+keyword = ''
+# some profile have a lot of content, here you can adjust how many screen scroll should be downloaded
+max_scroll_counter = 5
+# content type: post = posts, reels = reels
+content_type = "posts"
